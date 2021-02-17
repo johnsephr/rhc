bcf85a4ff767f4404753ad8f95bbfc72d26e8600
dextrodeath <ramseyjohnjoseph@gmail.com>
2021-02-16 19:02:38 -0600

    style adjustments

--- src/components/Who/Who.js
+++ src/components/Who/Who.js
@@ -4,25 +4,39 @@ import React, { } from 'react';
 import globals from '../../styles/globals';
 
 // styles
-const styles = {}
+const styles = {
+    body: {
+        width: '50%'
+    },
+    list: {
+        listStyle: 'none',
+        padding: 0,
+    },
+    listItem: {
+        paddingTop: 10,
+        paddingBottom: 10
+    }
+}
 
 const Who = props => {
     return <div style={globals.slideContainer}>
-        <h2 style={globals.header}>WHO</h2> is eligible<br />
+        <div><h2 style={globals.header}>WHO</h2> is eligible</div>
 
-        <p className=''>The following entities are eligible for funding provided through the Rural Health Care (RHC) Program</p>
+        <div style={styles.body}>
+            <p><strong>The following entities are eligible for funding provided through the Rural Health Care (RHC) Program:</strong></p>
 
-        <ul>
-            <li>Not-for-profit hospitals</li>
-            <li>Dedicated emergency rooms of rural for-profit hospitals</li>
-            <li>Local health departments or agencies</li>
-            <li>Community mental health centers</li>
-            <li>Rural health clinics</li>
-            <li>Community health centers or health care centers providing healthcare to migrants</li>
-            <li>Skilled nursing facilities (SNF)</li>
-            <li>Post-secondary educational institutions offering healthcare instruction, such as teaching hospitals or
-        medical schools</li>
-        </ul>
+            <ul style={styles.list}>
+                <li style={styles.listItem}>Not-for-profit hospitals</li>
+                <li style={styles.listItem}>Dedicated emergency rooms of rural for-profit hospitals</li>
+                <li style={styles.listItem}>Local health departments or agencies</li>
+                <li style={styles.listItem}>Community mental health centers</li>
+                <li style={styles.listItem}>Rural health clinics</li>
+                <li style={styles.listItem}>Community health centers or health care centers providing healthcare to migrants</li>
+                <li style={styles.listItem}>Skilled nursing facilities (SNF)</li>
+                <li style={styles.listItem}>Post-secondary educational institutions offering healthcare instruction, such as teaching hospitals or
+medical schools</li>
+            </ul>
+        </div>
     </div>
 };
 
